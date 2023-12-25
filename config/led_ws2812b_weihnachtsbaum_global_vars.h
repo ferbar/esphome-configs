2608dacd27f3f96bb1236346c11d1246c0077a56
Christian Ferbar <5595808+ferbar@users.noreply.github.com>
2023-12-25 22:15:16 +0100

    fix

--- config/led_ws2812b_weihnachtsbaum_global_vars.h
+++ config/led_ws2812b_weihnachtsbaum_global_vars.h
@@ -1,7 +1,11 @@
 // class LedPosInfo;
 void initLedPosInfoLoops(AddressableLight &it, int *loopLedStartNumbers, int *loopDiameter);
-static int loopLedStartNumbers[]={14,27,50,78,113,145,175 -1};
-static int loopDiameter[]={50,100,150,200,230,255,255};
+// wenn der letzte kreis nicht volständig ist dann ~ letztes kreis ende angeben
+// !! die erste Led ist natürlich beim doserl !!
+//static int loopLedStartNumbers[]={14,27,50,78,113,145,175 -1};
+static int loopLedStartNumbers[]={40,65,90,105,120,135,145,160, -1};
+// von aussen nach innen:
+static int loopDiameter[]={255,255,150,150,100,100,100};
 
 void initLedPosInfo(AddressableLight &it) {
 	initLedPosInfoLoops(it, loopLedStartNumbers, loopDiameter);
